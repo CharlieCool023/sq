31ac5400f67963e1eb6de9d3c74dff095283576f
charliecool023 <charlesonah023@gmail.com>
2026-02-18 17:20:51 +0100

    team

--- src/sections/Team.tsx
+++ src/sections/Team.tsx
@@ -34,7 +34,7 @@ const Team = () => {
         </div>
 
         {/* Team Grid */}
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
           {teamMembers.map((member, index) => (
             <Link
               to={`/team/${member.slug}`}
